9885645da7b0a31c32b2c107c9b2951d6ebc1bdb
VolZa <vvolza@gmail.com>
2023-06-24 07:48:27 +0300

    hw01-node

--- contacts.js
+++ contacts.js
@@ -1,5 +1,69 @@
-const fs = require("fs/promises");
+const fs = require("node:fs/promises");
 const path = require("path");
-
+const { v4: uuidv4 } = require('uuid');
+//Зберігаю нормалізований шлях до файла contacts.json
 const contactsPath = path.join(__dirname, "db/contacts.json");
-console.log(contactsPath)
+
+async function listContacts() {
+   // зчитую контакти з файла /db/contacts.json в кодуванні "utf8" 
+  const contacts = await fs.readFile(contactsPath, "utf8");
+  // return contacts;
+  return JSON.parse(contacts);
+};
+
+async function writeContacts(contacts) {
+  //writeContacts Викристовую лише в цьому файлі
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+}
+ 
+ async function getContactById(contactId) {
+   // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+   const contacts = await listContacts();
+   const contact = contacts.find((contact) => contact.id === contactId);
+
+  return contact || null;
+ }
+ 
+ async function removeContact(contactId) {
+   // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+   const contacts = await listContacts();
+   const index = contacts.findIndex(contact => contact.id === contactId);
+   const newContacts = [
+    ...contacts.slice(0, index),
+    ...contacts.slice(index + 1),
+   ];
+   await writeContacts(newContacts);
+   return newContacts || null;
+ }
+ 
+ async function addContact(name, email, phone) {
+   // ...твій код. Повертає об'єкт доданого контакту. 
+   const contacts = await listContacts();
+   const contact = {id:uuidv4(), "name":name, "email":email, "phone":phone}
+   contacts.push(contact); 
+   return contact;
+ }
+
+// console.log(listContacts());
+
+module.exports = { listContacts,
+  getContactById,
+  removeContact,
+  addContact
+ };
+
+ // TODO: задокументувати кожну функцію
+// fs.readFile(contactsPath, "utf8")
+//   .then((data) => console.log(data))
+//   .catch((err) => console.error(err));
+  
+  
+  
+
+
+// fs.writeFile(contactsPath, "utf8", [{}](err, data) => {
+//   if (err) throw err;
+  
+//   console.log(data);
+// });
+// const contacts = fs.readFile(contactsPath, "utf8");
--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-//index file
+const contacts = require("./contacts.js");
+const argv = require('yargs').argv;
+
+async function invokeAction({ action, id, name, email, phone }) {
+   switch (action) {
+     case 'list':
+       // ...
+       const contactL = await contacts.listContacts();
+       console.log(contactL);
+       break;
+ 
+     case 'get':
+       // ... id
+       const contact = await contacts.getContactById(id);
+       console.log(contact);
+       break;
+ 
+     case 'add':
+       // ... name email phone
+       const adedContact = await contacts.addContact(name, email, phone);
+       console.log(adedContact);
+       break;
+ 
+     case 'remove':
+       // ... id
+       const removedContact = removeContact(id);
+       console.log(removedContact);
+       break;
+ 
+     default:
+       console.warn('\x1B[31m Unknown action type! Ось так.');
+   }
+ }
+ 
+ invokeAction({action: "get", id:"qdggE76Jtbfd9eWJHrssH"}).then(console.log).catch(console.error);
+
+//  invokeAction({action: "list"}).then(console.log).catch(console.error);
+
+ 
+//  invokeAction(argv);
+ // contacts
+//    .listContacts()
+//    .then((contact) => console.log(contact))
+//    .catch((er) => console.error(er));
