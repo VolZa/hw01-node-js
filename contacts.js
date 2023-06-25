55ed37460c27dbc0f0b162973b3315ea1a47be26
VolZa <vvolza@gmail.com>
2023-06-25 21:50:48 +0300

    fix comments

--- contacts.js
+++ contacts.js
@@ -10,25 +10,23 @@ const contactsPath = path.join(__dirname, "db/contacts.json");
 async function listContacts() {
    // зчитую контакти з файла /db/contacts.json в кодуванні "utf8" 
   const contacts = await fs.readFile(contactsPath, "utf8");
-  // return contacts;
   return JSON.parse(contacts);
 };
 
+//writeContacts Викристовую лише в цьому файлі
 async function writeContacts(contacts) {
-  //writeContacts Викристовую лише в цьому файлі
   await fs.writeFile(contactsPath, JSON.stringify(contacts));
 }
- 
+ //Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
  async function getContactById(contactId) {
-   // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
    const contacts = await listContacts();
    const contact = contacts.find((contact) => contact.id === contactId);
 
   return contact || null;
  }
  
+//Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
 async function removeContact(contactId) {
-  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
   const contactsL = await listContacts();
   const index = contactsL.findIndex(contact => contact.id === contactId);
   const delContact = contactsL.filter(c => c.id === contactId);
@@ -41,15 +39,15 @@ async function removeContact(contactId) {
   await writeContacts(newContacts);
   return delContact  || null;
 }
- 
- async function addContact(name, email, phone) {
-   // ...твій код. Повертає об'єкт доданого контакту. 
-   const contacts = await listContacts();
-   const contact = {id:uuidv4(), "name":name, "email":email, "phone":phone}
-   contacts.push(contact);
-   await writeContacts(contacts);
-   return contact;
- }
+
+//Додає контакт та повертає об'єкт доданого контакту.
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const contact = {id:uuidv4(), "name":name, "email":email, "phone":phone}
+  contacts.push(contact);
+  await writeContacts(contacts);
+  return contact;
+}
 
 // Експорт функцій з модуля  contacts.js, щоб інші модулі або скрипти могли їх використовувати.
 module.exports = {
@@ -57,6 +55,6 @@ module.exports = {
   getContactById,
   removeContact,
   addContact
- };
+};
 
  
--- index.js
+++ index.js
@@ -30,34 +30,34 @@ const argv = require('yargs')
 
 
 async function invokeAction({ action, id, name, email, phone }) {
-   switch (action) {
-     case 'list':
-       // викликаю функцію що зчитує (з файла .json) список контактів та вивожу їх в консоль
-       const contactL = await contacts.listContacts();
-       console.table(contactL);
-       break;
- 
-     case 'get':
-       // викликаю функцію що зчитує (з файла .json) контакт за id та вивожу в консоль цей контакт
-       const contact = await contacts.getContactById(id);
-       console.log(contact);
-       break;
- 
-     case 'add':
-      // викликаю функцію що формує об"єкт контакт  та дописує його до списку контактів, виводжу цей контакт в консоль
-       const adedContact = await contacts.addContact(name, email, phone);
-       console.log(adedContact);
-       break;
- 
-     case 'remove':
-        // викликаю функцію що шукає в списку об"єкт контакт (за id) та видаляє його, в разі успішного пошуку, зі списку контактів. В разі успішного пошуку виводжу в консоль цей контакт, в разі неуспішного пошуку виводжу  null 
-       const removedContact = await contacts.removeContact(id);
-       console.log(removedContact);
-       break;
- 
-     default:
-       console.warn('\x1B[31m Unknown action type! Ось так.');
-   }
+  switch (action) {
+    case 'list':
+      // викликаю функцію що зчитує (з файла .json) список контактів та вивожу їх в консоль
+      const contactL = await contacts.listContacts();
+      console.table(contactL);
+      break;
+
+    case 'get':
+      // викликаю функцію що зчитує (з файла .json) контакт за id та вивожу в консоль цей контакт
+      const contact = await contacts.getContactById(id);
+      console.log(contact);
+      break;
+
+    case 'add':
+    // викликаю функцію що формує об"єкт контакт  та дописує його до списку контактів, виводжу цей контакт в консоль
+      const adedContact = await contacts.addContact(name, email, phone);
+      console.log(adedContact);
+      break;
+
+    case 'remove':
+      // викликаю функцію що шукає в списку об"єкт контакт (за id) та видаляє його, в разі успішного пошуку, зі списку контактів. В разі успішного пошуку виводжу в консоль цей контакт, в разі неуспішного пошуку виводжу  null 
+      const removedContact = await contacts.removeContact(id);
+      console.log(removedContact);
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type! Ось так.');
+  }
 }
 
 const { action, id, name, email, phone } = argv;
